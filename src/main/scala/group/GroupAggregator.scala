8fb2392017089ddfca5994331090ce6c9f3e8dfb
zhangshuai.21 <zhangshuai.21@bytedance.com>
2023-05-19 19:09:44 +0800

    up

--- src/main/scala/group/GroupAggregator.scala
+++ src/main/scala/group/GroupAggregator.scala
@@ -67,25 +67,132 @@ class GroupAggregator extends Serializable {
     this.hour = args.hour
     this.window = args.window
 
-    schemaItems = (groupNode \ "schema_list" \ "schema")
-      .map(_.text.trim)
-      .map{schemaStr => SchemaItem(schemaStr)}
-      .toArray
+    val schemaNodes = (groupNode \ "schema_list" \ "schema").map(_.text.trim)
+    if(schemaNodes.size > 0 ) {
+      schemaItems = schemaNodes
+        .map{schemaStr => SchemaItem(schemaStr)}
+        .toArray
+      println(schemaItems.mkString("|"))
+    }
 
-    println(schemaItems.mkString("|"))
 
     val countNodes = (groupNode \ "feature_list" \ "feature")
       .filter(x => (x \ "aggregator" \ "type").text.trim == "count")
 
-    countItems = countNodes.map{ x =>
-      var Array(fKey, space) = Array(x \ "fkey", x \ "space").map(_.text.trim)
-      if(space == "") {
-        space = "TRUE"
-      }
-      CountingItem(fKey, Utils.spaceParse(space))
-    }.toArray
+    if(countNodes.size > 0) {
+      countItems = countNodes.map { x =>
+        var Array(fKey, space) = Array(x \ "fkey", x \ "space").map(_.text.trim)
+        if(space == "") {
+          space = "TRUE"
+        }
+        CountingItem(fKey, Utils.spaceParse(space))
+      }.toArray
 
-    println(countItems.mkString("|"))
+      println(countItems.mkString("|"))
+    }
+
+
+    val disCountNodes = (groupNode \ "feature_list" \ "feature")
+      .filter(x => (x \ "aggregator" \ "type").text.trim == "distinct_count")
+
+    if(disCountNodes.size > 0) {
+      disCountItems = disCountNodes.map { x =>
+        var Array(fKey, space, distinctKey) = Array(x \ "fkey", x \ "space",
+          x \ "aggregator" \ "distinctkey").map(_.text.trim)
+
+        if(space == "") {
+          space = "TRUE"
+        }
+        DistinctItem(fKey, Utils.spaceParse(space), distinctKey)
+      }.toArray
+
+      println(disCountItems.mkString("|"))
+
+    }
+
+    val ratioNodes = (groupNode \ "feature_list" \ "feature")
+      .filter(x => (x \ "aggregator" \ "type").text.trim == "ratio")
+
+    if(ratioNodes.size > 0) {
+
+      ratioItems = ratioNodes.map { x =>
+
+        val Array(fKey, space, topSpace, bottomSpace) =
+          Array(x \ "fkey", x \ "space", x \ "aggregator" \ "top",
+            x \ "aggregator" \ "bottom").map(_.text.trim)
+
+        val spaceStr = if(space == "") "TRUE" else space
+
+        val topSpaceStr = if(topSpace == "") "TRUE" else topSpace
+
+        val bottomSpaceStr = if(bottomSpace == "") "TRUE" else bottomSpace
+
+        RatioItem(fKey, Utils.spaceParse(spaceStr),
+          Utils.spaceParse(topSpaceStr), Utils.spaceParse(bottomSpaceStr))
+
+      }.toArray
+
+      println(ratioItems.mkString("|"))
+
+    }
+
+
+    val disRatioNodes = (groupNode \ "feature_list" \ "feature")
+      .filter(x => (x \ "aggregator" \ "type").text.trim == "distinct_ratio")
+
+      if(disRatioNodes.size > 0) {
+        disRatioItems = disRatioNodes.map { x =>
+
+        val Array(fKey, space, distinctkey, topSpace, bottomSpace) =
+          Array(x \ "fkey", x \ "space", x \ "distinctkey", x \ "aggregator" \ "top",
+            x \ "aggregator" \ "bottom").map(_.text.trim)
+
+        val spaceStr = if(space == "") "TRUE" else space
+
+        val topSpaceStr = if(topSpace == "") "TRUE" else topSpace
+
+        val bottomSpaceStr = if(bottomSpace == "") "TRUE" else bottomSpace
+
+        DisRatioItem(fKey, Utils.spaceParse(spaceStr), distinctkey,
+          Utils.spaceParse(topSpaceStr), Utils.spaceParse(bottomSpaceStr))
+       }.toArray
+
+        println(disRatioItems.mkString("|"))
+      }
+
+
+    val statsNodes = (groupNode \ "feature_list" \ "feature")
+      .filter(x => (x \ "aggregator" \ "type").text.trim == "stats")
+
+    if(statsNodes.size > 0) {
+
+      statsItems = statsNodes.map { x =>
+        val Array(fKey, space, func, statsKey, truncate) = Array(
+          x \ "fkey",
+          x \ "space",
+          x \ "aggregator" \ "func",
+          x \ "aggregator" \ "statskey",
+          x \ "aggregator" \ "truncate"
+        ).map(_.text.trim)
+
+        val spaceStr = if(space == "" ) "TRUE" else space
+
+        StatsItem(
+          fKey,
+          Utils.spaceParse(spaceStr),
+          func.toUpperCase,
+          statsKey,
+          if(truncate != "") truncate.toFloat else -1.0F)
+
+      }.toArray
+
+      println(statsItems.mkString("|"))
+
+    }
+
+
+
+
 
 
   }
@@ -94,6 +201,7 @@ class GroupAggregator extends Serializable {
 
     ""
   }
+
   def compute(spark:SparkSession): Unit = {
     println("hello compute()")
   }
